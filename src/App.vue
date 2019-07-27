ba2a33daa8f35d6b0a9730afa3c295e90cc90f78
HUANBIAN <1768400228@qq.com>
2019-07-27 18:23:35 +0800

    测试002

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <router-view></router-view>
 
     <p>ceshibianjiqiengbnebgtijiaoxinxi</p>
+    <p>gangcaikancuole</p>
 
     <nav class="mui-bar mui-bar-tab">
       <router-link class="mui-tab-item" to="/home">
