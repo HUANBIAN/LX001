2fd99766afe3274672acbea28c484bb643bc76a8
HUANBIAN <1768400228@qq.com>
2019-07-27 17:33:56 +0800

    引入axios

--- src/components/tabbar/Home.vue
+++ src/components/tabbar/Home.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-        homezujian
-    </div>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
+    <mt-button type="primary" size="normal">点击</mt-button>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {};
+  },
+  created() {
+    // this.getLunbotu();
+  },
+  methods: {
+    // getLunbotu() {
+    //   this.$http
+    //     .get("http://vue.studyit.io/api/getnewslist")
+    //     .then(function(response) {
+    //       console.log(response);
+    //     })
+    //     .catch(function(error) {
+    //       console.log(`错误信息：${error}`);
+    //     });
+    // }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.mint-swipe {
+  height: 200px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -12,17 +12,23 @@ import router from "./router.js";
 import app from "./App.vue";
 
 // 导入mint-mi
-// import MintUI from 'mint-ui';
-// import 'mint-ui/lib/style.css';
-// Vue.use(MintUI);
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+Vue.use(MintUI);
 
 //导入mui
 import "./lib/mui/css/mui.min.css";
 import "./lib/mui/css/icons-extra.css";
 
+// 引入 axios
+import axios from 'axios';
+axios.defaults.baseURL = "https://www.ifilm.ltd/api/";
+// 将axios绑定给vue成为一个属性
+Vue.prototype.$http = axios;
 
 var vm = new Vue({
   el: "#app",
   render: c => c(app),
-  router
+  router,
+  axios
 })
